a615cfc1f36a544ffe0aafb1edb0ac89764921bc
NguyenKongDai <kongdaittnbk@gmail.com>
2022-05-17 22:43:01 +0700

    test

--- Source/Server/server.py
+++ Source/Server/server.py
@@ -8,7 +8,7 @@ import socket
 import numpy as np
 import json
 
-HOST = '10.0.29.36'  # Standard loopback interface address (localhost)
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 12345        # Port to listen on (non-privileged ports are > 1023)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
